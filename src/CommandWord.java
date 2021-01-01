dcbc5053702e3d2a7a25078acda3e9ca84287382
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-01-01 15:10:45 +0100

    Add CommandWord Enumeration class representing Command input from player

---
+++ src/CommandWord.java
@@ -0,0 +1,24 @@
+/**
+ * Enumeration class CommandWord models a command input from the user. The class
+ * contains a field commandString representing the command.
+ * 
+ * @author Alexander Mertens
+ */
+public enum CommandWord {
+    GO("go"), TAKE("take"), DROP("drop"), BACK("back"), QUIT("quit"), HELP("help"), LOOK("look"), UNKNOWN("?");
+
+    private String commandString;
+
+    /**
+     * Constructs a CommandWord with the given commandString.
+     * 
+     * @param commandString
+     */
+    CommandWord(String commandString) {
+        this.commandString = commandString;
+    }
+
+    public String toString() {
+        return commandString;
+    }
+}
