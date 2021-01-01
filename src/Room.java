a90f8543937fa945695e2832a77e0497617585a9
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-01-01 14:50:10 +0100

    Add Room class that represents a room in the maze
    Room extends Container class, so a room can hold items.
    In addition the Room class implements functionality to set exits
    to other Room instances in given Directions.

---
+++ src/Room.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+
+/**
+ * The Room class models a GameObject that represents a single room in the maze.
+ * The Room class extends the Container class, because a Room can contain items.
+ * An instance of Room has a HashMap field named exits with Directions as keys
+ * and other instances of Room as values. The class implements methods to set an
+ * exit in a certain direction or retrieve a room that is located in a direction
+ * with respect to this room.
+ */
+public class Room extends Container {
+    private HashMap<Direction, Room> exits;
+
+    /**
+     * Constructs an empty room with no exits.
+     * 
+     * @param name        Name of the room.
+     * @param description Description of the room.
+     */
+    public Room(String name, String description) {
+        super(name, description);
+        exits = new HashMap<>();
+    }
+
+    /**
+     * Adds or overwrites an exit of this room. The direction of the exit is set
+     * with the given parameter.
+     * 
+     * @param direction Direction of the exit.
+     * @param exit      Room object that represents the exit.
+     */
+    public void setExit(Direction direction, Room exit) {
+        exits.put(direction, exit);
+    }
+
+    /**
+     * Retrieves an exit of this room in the given direction. Returns null if
+     * there's no Room in the given direction.
+     * 
+     * @param direction Direction of the Room to be retrieved.
+     * @return Returns Room object in the given direction.
+     */
+    public Room getExit(Direction direction) {
+        return exits.get(direction);
+    }
+}
