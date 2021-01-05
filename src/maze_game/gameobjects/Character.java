be55c114b9d54e8660a220bfc68bea7008547dde
Alexander Mertens <alexander.mertens@student.thomasmore.be>
2021-01-05 18:21:24 +0100

    Implement Character class

---
+++ src/maze_game/gameobjects/Character.java
@@ -0,0 +1,48 @@
+package maze_game.gameobjects;
+
+/**
+ * This class represents a character in the game world. A character has a string
+ * of dialogue for when it meets the player, and another string of dialogue for
+ * afterwards.
+ * 
+ * @author Alexander Mertens
+ */
+public class Character extends GameObject {
+    private String initialDialogue;
+    private String defaultDialogue;
+    // Records whether the character has met the player or not.
+    private boolean hasMet;
+
+    /**
+     * Creates a character with given name, description and dialogue.
+     * 
+     * @param name            Name of character.
+     * @param description     Description of character.
+     * @param initialDialogue Dialogue upon meeting of the character.
+     * @param defaultDialogue Dialogue after meeting of the character.
+     */
+    public Character(String name, String description, String initialDialogue, String defaultDialogue) {
+        super(name, description);
+        this.initialDialogue = initialDialogue;
+        this.defaultDialogue = defaultDialogue;
+        this.hasMet = false;
+    }
+
+    /**
+     * Returns a String with the dialogue the character says. This can vary
+     * depending on whether the character has already met the player.
+     * 
+     * @return A String containing the appropriate dialogue.
+     */
+    public String getDialogue() {
+        if (hasMet) {
+            return defaultDialogue;
+        } else {
+            return initialDialogue;
+        }
+    }
+
+    public void meet() {
+        this.hasMet = true;
+    }
+}
